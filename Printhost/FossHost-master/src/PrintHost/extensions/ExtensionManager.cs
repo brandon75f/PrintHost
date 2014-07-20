21dff4af165f87b91b3347a42db9bb40cae687ff
brandon75f <brandon@fossrobotics.com>
2014-07-20 15:41:10 -0600

    first commit

---
+++ Printhost/FossHost-master/src/PrintHost/extensions/ExtensionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintHost.extensions
+{
+    public class ExtensionManager
+    {
+        public static void Initalize()
+        {
+        }
+    }
+}
